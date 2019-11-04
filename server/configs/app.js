7e732bda2391bd0bd8a61b1d683d3964c2f3ac49
Maxim <maxa@ciklum.com>
2019-11-04 15:21:10 +0200

    SJ-155: + mongo uri env

--- server/configs/app.js
+++ server/configs/app.js
@@ -2,5 +2,5 @@ const argv = require('../util/argv');
 
 module.exports = {
   port: parseInt(argv.port || process.env.PORT || '3000', 10),
-  dbHost: 'mongodb://mongo:27017/ci-cd-products'
+  dbHost: process.env.MONGO_DB_URI || 'mongodb://localhost:27017/ci-cd-products'
 };
