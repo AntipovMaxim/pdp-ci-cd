fb9fa2eb84074f80e5d130a01b9d2548cf93889e
Maxim <maxa@ciklum.com>
2019-11-05 16:54:02 +0200

    failed tests

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './style.scss';
 
 const Footer = () => (
   <footer>
-    <section><b>PDP CI/CD project. 2019</b></section>
+    <section><b>PDP CI/CD project. 2019asvsdvvds</b></section>
   </footer>
 );
 
