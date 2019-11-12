07f44c092e615d2c17136ad9892748334523dc4a
Maxim <maxa@ciklum.com>
2019-11-13 00:34:04 +0200

    SJ-165: test automerge6

--- devops/scripts/auto_merge.sh
+++ devops/scripts/auto_merge.sh
@@ -1,4 +1,8 @@
 echo "AUTO_MERGE"
+
+export GIT_COMMITTER_EMAIL="antipov.maxim88@gmail.com"
+export GIT_COMMITTER_NAME="Maxim Antipov"
+
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 
@@ -7,5 +11,5 @@ git checkout master || exit
 git merge --no-ff origin/feature/SJ-165-automerge || exit
 
 
-git push https://7fbd6d26fa61d3145eaf6aa3b59b483099642c01@github.com/martmax/pdp-ci-cd.git
+git push https://189d7f0ef63aa17bdca9c557a5827906d9cdcf20@github.com/martmax/pdp-ci-cd.git
 
