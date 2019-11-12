58912f4dd35661adfc74124031261329c0b20a0b
Maxim <maxa@ciklum.com>
2019-11-13 00:10:12 +0200

    SJ-165: test automerge3

--- devops/scripts/auto_merge.sh
+++ devops/scripts/auto_merge.sh
@@ -1,12 +1,11 @@
-printf '1>>>>>>>>>>>>>.\n'
+echo "AUTO_MERGE"
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 
-printf '\nb2>>>>>>>>>>>>>>'
+
 git checkout master || exit
 git merge --no-ff origin/For-testing || exit
 
-printf '3>>>>>>>>>\n'
+
 git push https://7fbd6d26fa61d3145eaf6aa3b59b483099642c01@github.com/martmax/pdp-ci-cd.git
 
-printf '4>>>>>>>>>>>>n'
