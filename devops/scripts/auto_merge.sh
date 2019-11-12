29bcae85465a61eec7b8afa577115bf1d5a96458
Maxim <maxa@ciklum.com>
2019-11-13 00:44:09 +0200

    SJ-165: test automerge7

--- devops/scripts/auto_merge.sh
+++ devops/scripts/auto_merge.sh
@@ -11,5 +11,5 @@ git checkout master || exit
 git merge --no-ff origin/feature/SJ-165-automerge || exit
 
 
-git push https://189d7f0ef63aa17bdca9c557a5827906d9cdcf20@github.com/martmax/pdp-ci-cd.git
+git push https://martmax:189d7f0ef63aa17bdca9c557a5827906d9cdcf20@github.com/martmax/pdp-ci-cd.git
 
