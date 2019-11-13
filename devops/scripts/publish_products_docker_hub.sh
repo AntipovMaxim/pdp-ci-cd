2187f7c0835423209904eafe72896d409888b9e3
Maxim <maxa@ciklum.com>
2019-11-14 01:55:19 +0200

    SJ-165: publish to ecr

--- devops/scripts/publish_products_docker_hub.sh
+++ devops/scripts/publish_products_docker_hub.sh
@@ -2,10 +2,9 @@ echo "Branch: $TRAVIS_BRANCH"
 echo "Pull request: $TRAVIS_PULL_REQUEST"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    echo "Publish for Products-service to DOCKER HUB started..."
+    echo "START Publish for Products-service to DOCKER HUB"
 
     currentDir=$PWD
-    echo "Current directory $currentDir"
 
     docker login -u="$DOCKER_HUB_USERNAME" -p="$DOCKER_HUB_PASSWORD" || exit 1
 
