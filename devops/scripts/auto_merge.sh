154e21662644cb1ff0c04aefcaf628aab376f8d2
Maxim <maxa@ciklum.com>
2019-11-14 13:36:46 +0200

    SG-165: + automerg

--- devops/scripts/auto_merge.sh
+++ devops/scripts/auto_merge.sh
@@ -12,4 +12,15 @@ git merge --no-ff origin/feature/SJ-165-automerge || exit
 
 
 git push https://dddd:github.com/martmax/pdp-ci-cd.git
+export GIT_COMMITTER_EMAIL="travis@travis"
+export GIT_COMMITTER_NAME="Travis CI"
 
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+
+git fetch --all || exit
+
+git checkout master || exit
+
+git merge --no-ff "$TRAVIS_COMMIT" || exit
+
+git push "https://$GITHUB_SECRET_TOKEN@github.com/martmax/pdp-ci-cd.git"
