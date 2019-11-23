a057589910b814c87b53c6832476e3604a266f7c
Maxim <maxa@ciklum.com>
2019-11-23 16:30:30 +0200

    SJ-165: ! script 3

--- devops/scripts/build-condition.sh
+++ devops/scripts/build-condition.sh
@@ -3,14 +3,19 @@
 echo "Commit range - $TRAVIS_COMMIT_RANGE $1"
 echo " Target -  $TARGET $2"
 
-if [[ -z $1 ]]; then
+if [[ -z $TRAVIS_COMMIT_RANGE ]]; then
     echo "Commit range cannot be empty"
     exit 1
 fi
 
-if [[ -z $2 ]]; then
+if [[ -z $TARGET ]]; then
     echo "Change path cannot be empty"
     exit 1
 fi
 
-git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
+
+
+if [[ git diff --name-only $TRAVIS_COMMIT_RANGE | sort -u | uniq | grep $TARGET > /dev/null ]]; then
+    echo "$TARGET BUILLLLLLLLLDDDDDDDDD"
+fi
+
