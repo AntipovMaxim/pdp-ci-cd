c5b2800e0f2ccfb0c82b89f2904ce02b54f4d31f
Maxim <maxa@ciklum.com>
2019-11-23 23:57:25 +0200

    SJ-165: ! script 23

--- devops/scripts/ci.run-all.sh
+++ devops/scripts/ci.run-all.sh
@@ -1,6 +1,1 @@
-currentDir=$PWD
-
-DETECT_CHANGES_SCRIPT="$currentDir/devops/scripts/check-target-changes.sh"
-RUN_TARGET_SCRIPT="$currentDir/devops/scripts/check-target-changes.sh"
-
-if DETECT_CHANGES_SCRIPT; then RUN_TARGET_SCRIPT; else echo "$TARGET - IS NOT BEING CHANGED"; fi
+if ./devops/scripts/check-target-changes.sh; then ./devops/scripts/ci.run-target.sh; else echo "$TARGET - IS NOT BEING CHANGED"; fi
