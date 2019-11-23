1fba8e3a4483814e2c4d7f4142ac7c0e004b87b0
Maxim <maxa@ciklum.com>
2019-11-23 22:58:45 +0200

    SJ-165: ! script 19

--- devops/scripts/ci.run.lint.sh
+++ devops/scripts/ci.run.lint.sh
@@ -1,1 +1,1 @@
-if ./build-condition.sh $TRAVIS_COMMIT_RANGE $TARGET; then ./ci.lint.split.sh $TARGET; else echo "$TARGET is NOT being built"; fi
+if ./devops/scripts/build-condition.sh $TRAVIS_COMMIT_RANGE $TARGET; then ./devops/scripts/ci.lint.split.sh $TARGET; else echo "$TARGET is NOT being built"; fi
