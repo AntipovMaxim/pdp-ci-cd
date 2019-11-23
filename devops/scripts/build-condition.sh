2fbca10b90c7fbbb9acc9481d478256e35904a25
Maxim <maxa@ciklum.com>
2019-11-23 17:09:37 +0200

    SJ-165: ! script 8

--- devops/scripts/build-condition.sh
+++ devops/scripts/build-condition.sh
@@ -16,9 +16,9 @@ CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
 
 echo CHANGED_FILES
 
+TTT=git diff --name-only $TRAVIS_COMMIT_RANGE | sort -u | uniq | grep $TARGET > /dev/null
 
-
-if [[ git diff --name-only $TRAVIS_COMMIT_RANGE | sort -u | uniq | grep $TARGET > /dev/null ]]; then
+if [[ TTT ]]; then
   echo "$TARGET BUILLLLLLLLLDDDDDDDDD"
 else
   echo "$TARGET NOOOOOOOT BUILLLLLLLLLDDDDDDDDD"
