06d2e3d7c37d841fde7012ded02850a7dd70d05a
Maxim <maxa@ciklum.com>
2019-11-25 15:02:05 +0200

    SJ-166: after success py5

--- devops/scripts/after_success.sh
+++ devops/scripts/after_success.sh
@@ -1,7 +1,2 @@
-if [ "$BUILD_LEADER" = "YES" ]; then
-        if [ "$BUILD_AGGREGATE_STATUS" = "others_succeeded" ]; then
-          echo "All jobs succeeded! PUBLISHING..."
-        else
-          echo "Some jobs failed"
-        fi
-      fi
+echo $BUILD_LEADER
+echo $BUILD_AGGREGATE_STATUS
