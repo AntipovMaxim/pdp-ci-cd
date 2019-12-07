d09cfe355e56427efae2d1a91ebad16e56e15f8c
Maxim <maxa@ciklum.com>
2019-12-07 17:28:45 +0200

    SJ-166: * travis6

--- devops/scripts/ci.lint.sh
+++ devops/scripts/ci.lint.sh
@@ -1,3 +1,14 @@
-if ./devops/scripts/check-target-changes.sh client; then ./devops/scripts/ci.run-target.lint.sh client; else echo "client app - IS NOT BEING CHANGED"; fi
-if ./devops/scripts/check-target-changes.sh auth-service; then ./devops/scripts/ci.run-target.lint.sh auth-service; else echo "auth-service app - IS NOT BEING CHANGED"; fi
-if ./devops/scripts/check-target-changes.sh products-service; then ./devops/scripts/ci.run-target.lint.sh products-service; else echo "products-service app - IS NOT BEING CHANGED"; fi
+#! /bin/bash
+
+function lintApps() {
+   arr=("$@")
+   for i in "${arr[@]}";
+      do
+          if ./devops/scripts/check-target-changes.sh $i; then ./devops/scripts/ci.run-target.lint.sh $i; else echo "$i app - IS NOT BEING CHANGED"; fi
+      done
+
+}
+
+IFS='/' read -r -a apps <<< "$APPS"
+
+lintApps "${apps[@]}"
