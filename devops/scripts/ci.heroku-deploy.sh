400c359bb96d65dffdbc10f676c1c1e372dc12df
Maxim <maxa@ciklum.com>
2019-12-07 17:59:35 +0200

    SJ-166: * travis8

--- devops/scripts/ci.heroku-deploy.sh
+++ devops/scripts/ci.heroku-deploy.sh
@@ -1,1 +1,14 @@
-if ./devops/scripts/check-target-changes.sh $1; then ./devops/scripts/ci.heroku-deploy-target.sh $1; else echo "$1 - IS NOT BEING CHANGED"; fi
+#! /bin/bash
+
+function deployApps() {
+   arr=("$@")
+   for i in "${arr[@]}";
+      do
+          if ./devops/scripts/check-target-changes.sh $i; then ./devops/scripts/ci.heroku-deploy-target.sh $i; else echo "$i app - IS NOT BEING CHANGED"; fi
+      done
+
+}
+
+IFS='/' read -r -a apps <<< "$APPS"
+
+deployApps "${apps[@]}"
